8a53bed3ced0cbde6f6b5c4c826daffdbc4ef819
whizthom <bamiseemmanuel137@gmail.com>
2026-08-25 12:58:15 +0100

    Remove venv and .env from tracking, add .gitignore

--- chatapi/Lib/site-packages/langchain_google_genai/_version.py
+++
@@ -1,8 +0,0 @@
-"""Package version for `langchain-google-genai`."""
-
-from importlib import metadata
-
-try:
-    __version__ = metadata.version(__package__)
-except metadata.PackageNotFoundError:
-    __version__ = ""
